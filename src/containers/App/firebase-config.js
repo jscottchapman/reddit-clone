81332512e9c012708eb2f947bbda77d3785839fe
J Scott Chapman <scott8675309@gmail.com>
2018-06-28 22:01:59 -0600

    Delete secrets

--- src/containers/App/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyAhVUOy-XHpoD5p1Sj5ufN55rW1WgqSsXo",
-    authDomain: "reddit-clone-e4a89.firebaseapp.com",
-    databaseURL: "https://reddit-clone-e4a89.firebaseio.com",
-    projectId: "reddit-clone-e4a89",
-    storageBucket: "reddit-clone-e4a89.appspot.com",
-    messagingSenderId: "232786206393"
-  };
-
